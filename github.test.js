ab047f58827b30eb780d4f6d8f5c5c9624f17d16
Ian Fitzpatrick <ian.fitzpatrick6@gmail.com>
2018-11-27 13:08:00 +0000

    Adding a note about mocks in jest

--- github.test.js
+++ github.test.js
@@ -1,5 +1,9 @@
 import github from "./github";
 
+
+// Manual mocks are defined by writing a module in a __mocks__/ subdirectory immediately adjacent to the module.
+// For example, to mock a module called user in the models directory, create a file called user.js and put it in the models/__mocks__ directory.
+// Note that the __mocks__ folder is case-sensitive, so naming the directory __MOCKS__ will break on some systems.
 // jest.mock("./request");
 
 describe("should get user from github", () => {
